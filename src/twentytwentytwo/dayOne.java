8acaa5b24003e882c7e972842b078c7b5af9d701
Martyn Taylor-Sherwood <martyn@taylorsherwood.co.uk>
2022-12-01 06:57:07 +0000

    2022 Day One

--- src/twentytwentytwo/dayOne.java
+++ src/twentytwentytwo/dayOne.java
@@ -3,6 +3,8 @@ package twentytwentytwo;
 import utils.days;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class dayOne extends days {
     private final static String year = "2022";
@@ -18,12 +20,50 @@ public class dayOne extends days {
     }
 
     private static void puzzleOne() {
-        int answer = 0;
-        System.out.println("Puzzle One: " + answer);
+        int rations = 0;
+        int biggestRation = 0;
+
+        while (input.hasNextLine()) {
+            String line = input.nextLine();
+            if (line.equals("")) {
+                if (rations > biggestRation) {
+                    biggestRation = rations;
+                }
+                rations = 0;
+            } else {
+                try {
+                    rations += Integer.parseInt(line);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+        System.out.println("Puzzle One: " + biggestRation);
     }
 
     private static void puzzleTwo() {
-        int answer = 0;
+        int rations = 0;
+        int answer;
+        final ArrayList<Integer> rationList = new ArrayList<>();
+
+        while (input.hasNextLine()) {
+            String line = input.nextLine();
+            if (line.equals("")) {
+                rationList.add(rations);
+                rations = 0;
+            } else {
+                try {
+                    rations += Integer.parseInt(line);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+        rationList.sort(Collections.reverseOrder());
+        answer = (rationList.get(0) + rationList.get(1) + rationList.get(2));
         System.out.println("Puzzle Two: " + answer);
     }
 }
