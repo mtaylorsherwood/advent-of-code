a8380e8b474d10e0a6f1058cefac4d25edf5fcbb
Martyn Taylor-Sherwood <martyn@taylorsherwood.co.uk>
2024-11-30 18:09:42 +0000

    Prep 2024

---
+++ src/twentytwentyfour/dayOne.java
@@ -0,0 +1,31 @@
+package twentytwentyfour;
+
+import utils.days;
+
+import java.io.IOException;
+
+public class dayOne extends days {
+    private final static String year = "2024";
+    private final static String day = "01";
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Running day " + day + " puzzles");
+        days.refreshScanner(year, day);
+        puzzleOne();
+        days.refreshScanner(year, day);
+        puzzleTwo();
+        System.out.println("END");
+    }
+
+    private static void puzzleOne() {
+        int answer = 0;
+
+        System.out.println("Puzzle One: " + answer);
+    }
+
+    private static void puzzleTwo() {
+        int answer = 0;
+
+        System.out.println("Puzzle Two: " + answer);
+    }
+}
