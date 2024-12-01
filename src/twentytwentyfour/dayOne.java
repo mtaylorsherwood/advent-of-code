02efce43640c4825c91cc3eea5396e96557ff14f
Martyn Taylor-Sherwood <martyn@taylorsherwood.co.uk>
2024-12-01 08:54:04 +0000

    2024 Day One Solution

--- src/twentytwentyfour/dayOne.java
+++ src/twentytwentyfour/dayOne.java
@@ -3,6 +3,8 @@ package twentytwentyfour;
 import utils.days;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 
 public class dayOne extends days {
@@ -26,12 +28,56 @@ public class dayOne extends days {
 
     private static void puzzleOne() {
         int answer = 0;
+        final ArrayList<Integer> left = new ArrayList<>();
+        final ArrayList<Integer> right = new ArrayList<>();
+        int lineNo = 1;
+
+        while (input.hasNextLine()) {
+            final String line = input.nextLine();
+            for (String c : line.split(" {3}")) {
+                if (lineNo % 2 == 0) {
+                    right.add(Integer.parseInt(c));
+                } else {
+                    left.add(Integer.parseInt(c));
+                }
+                lineNo++;
+            }
+        }
+
+        Collections.sort(left);
+        Collections.sort(right);
+
+        for (int i = 0; i < 1000; i++) {
+            int leftNum = left.get(i);
+            int rightNum = right.get(i);
+            answer += (leftNum > rightNum) ? leftNum - rightNum : rightNum - leftNum;
+        }
 
         System.out.println("Puzzle One: " + answer);
     }
 
     private static void puzzleTwo() {
         int answer = 0;
+
+        final ArrayList<Integer> left = new ArrayList<>();
+        final ArrayList<Integer> right = new ArrayList<>();
+        int lineNo = 1;
+
+        while (input.hasNextLine()) {
+            final String line = input.nextLine();
+            for (String c : line.split(" {3}")) {
+                if (lineNo % 2 == 0) {
+                    right.add(Integer.parseInt(c));
+                } else {
+                    left.add(Integer.parseInt(c));
+                }
+                lineNo++;
+            }
+        }
+
+        for (int location : left) {
+            answer += (location * Collections.frequency(right, location));
+        }
 
         System.out.println("Puzzle Two: " + answer);
     }
